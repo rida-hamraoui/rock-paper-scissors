44c0786ed03e146fd797938bd8fbef9cd14a0ff9
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-07 23:15:13 +0100

    Add playGame and playRound functions
    playRound function that get that computer and human choices and return the winner.
    playGame function make a game with 5 rounds.

--- game.js
+++ game.js
@@ -21,3 +21,34 @@ function getHumanChoice(){
 let humanScore = 0;
 let computerScrore = 0;
 
+// Function get result of a game round betwen human and computer
+function playRound(){
+    let computer = getComputerChoice();
+    let human = getHumanChoice();
+
+    if(human.toLowerCase() == computer.toLowerCase()){
+        console.log("It's a draw");
+    }else if(human.toLowerCase() == "paper" && computer.toLowerCase() == "scissors"){
+        console.log("You lose! Scissors beats Paper”");
+        computerScrore++;
+    }else if(human.toLowerCase() == "rock" && computer.toLowerCase() == "paper"){
+        console.log("You lose! Paper beats Rock”");
+        computerScrore++;
+    }else if(human.toLowerCase() == "scissors" && computer.toLowerCase() == "rock"){
+        console.log("You lose! Rock beats Scissors”");
+        computerScrore++;
+    }else{
+        console.log("You Win :)");
+        humanScore++;
+    }
+}
+
+function playGame(){
+    for(let i = 0; i < 5 ; i++){
+        playRound();
+    }
+}
+
+console.log(playGame());
+console.log(computerScrore)
+console.log(humanScore)

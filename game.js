4a46a49bab94fde22bc59ed50bf612fb6fa49b4a
RIDA HAMRAOUI <141268012+rida-hamraoui@users.noreply.github.com>
2025-03-10 15:04:47 +0100

    Make a ui for the game
    Add a button for each selection. Add events for the buttons.
    Add a display div to display the score, the result of each round and announce the winner of the game.

--- game.js
+++ game.js
@@ -12,43 +12,87 @@ function getComputerChoice() {
     return result;
 }
 
-// Function to get human choice
-function getHumanChoice(){
-    let choice = prompt("Enter your choice : ");
-    return choice;
+let humanScore = 0;
+let computerScore = 0;
+
+// Change the displayed result 
+function displayResult(message){
+    roundResult.textContent = message;
 }
 
-let humanScore = 0;
-let computerScrore = 0;
-
 // Function get result of a game round betwen human and computer
-function playRound(){
+function playRound(human){
     let computer = getComputerChoice();
-    let human = getHumanChoice();
 
     if(human.toLowerCase() == computer.toLowerCase()){
-        console.log("It's a draw");
+        displaResult("It's a draw");
     }else if(human.toLowerCase() == "paper" && computer.toLowerCase() == "scissors"){
-        console.log("You lose! Scissors beats Paper”");
-        computerScrore++;
+        displayResult("You lose! Scissors beats Paper");
+        computerScore++;
     }else if(human.toLowerCase() == "rock" && computer.toLowerCase() == "paper"){
-        console.log("You lose! Paper beats Rock”");
-        computerScrore++;
+        displayResult("You lose! Paper beats Rock");
+        computerScore++;
     }else if(human.toLowerCase() == "scissors" && computer.toLowerCase() == "rock"){
-        console.log("You lose! Rock beats Scissors”");
-        computerScrore++;
+        displayResult("You lose! Rock beats Scissors");
+        computerScore++;
     }else{
-        console.log("You Win :)");
+        displayResult("You Win :)");
         humanScore++;
     }
 }
 
-function playGame(){
-    for(let i = 0; i < 5 ; i++){
-        playRound();
+// Update display score
+function updateScore(){
+    hScore.textContent = "Your score : " + humanScore;
+    cScore.textContent = "Computer Score: " + computerScore;
+}
+
+// Check the winner
+function announceWinner(){
+    if(humanScore == 5){
+        displayResult("You Win The Game :)");
+        init();
+    }
+    if(computerScore == 5){
+        displayResult("You lose The Game :(");
+        init();
     }
 }
 
-console.log(playGame());
-console.log(computerScrore)
-console.log(humanScore)
+// initialize the score 
+function init(){
+    computerScore = 0;
+    humanScore = 0;
+    updateScore();
+}
+
+// Add a div to display the score
+const body = document.body;
+body.setAttribute("style", "margin: 10px; font-size: 30px;");
+const div = document.createElement("div");
+div.setAttribute("style", "border: solid black; padding: 10px;");
+body.appendChild(div);
+
+// Add event for each button
+const btns = document.querySelectorAll("button");
+btns.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.id);
+        updateScore();
+        announceWinner();
+    });
+});
+
+// Add the human score
+const hScore = document.createElement("div");
+hScore.textContent = "Your score : " + humanScore;
+div.appendChild(hScore);
+
+// Add the computer score
+const cScore = document.createElement("div");
+cScore.textContent = "Computer Score: " + computerScore;
+div.appendChild(cScore);
+ 
+// Display the result of the round or the game
+const Result = document.createElement("div");
+body.appendChild(Result);
